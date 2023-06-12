3673c950057fcbdf98a801b38bb2165c5c5b5be7
chandraauliatama <chandraaulia4@gmail.com>
2023-06-13 00:06:40 +0700

    little fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use App\Models\StaticImage;
 use Filament\Facades\Filament;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,11 +23,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $staticImages = StaticImage::all()->mapWithKeys(function($image) {
-            return [$image->label => $image->source];
-        });
-        view()->share('staticImages', $staticImages);
-
+        if (Schema::hasTable('static_images')) {
+            $staticImages = StaticImage::all()->mapWithKeys(function($image) {
+                return [$image->label => $image->source];
+            });
+            
+            view()->share('staticImages', $staticImages);
+        } 
+        
         Filament::serving(function () {
             // Using Vite
             Filament::registerViteTheme('resources/css/filament.css');
